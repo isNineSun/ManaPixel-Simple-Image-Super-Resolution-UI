a30ead01c866f6602378621ba5248fe5a01de70d
2769762037@qq.com <2769762037@qq.com>
2024-01-25 02:28:19 +0800

    增添拖入图片（或文件夹）功能

--- ManaPixel/MainWindow.xaml.cs
+++ ManaPixel/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Security.Policy;
+using Microsoft.Win32;
 
 namespace ManaPixel
 {
@@ -26,19 +29,81 @@ namespace ManaPixel
             InitializeComponent();
         }
 
-        private void Grid_DragEnter(object sender, DragEventArgs e)
+        // 设置 Border 的背景为指定图片
+        private void SetBorderBackground(string imagePath)
+        {
+            if (File.Exists(imagePath))
+            {
+                BitmapImage bitmap = new BitmapImage(new Uri(imagePath));
+                ImageBrush imageBrush = new ImageBrush(bitmap);
+                imageBrush.Stretch = Stretch.UniformToFill;
+                InputBox.Background = imageBrush;
+            }
+            else
+            {
+                BitmapImage bitmap = new BitmapImage(new Uri(@imagePath));
+                ImageBrush imageBrush = new ImageBrush(bitmap);
+                imageBrush.Stretch = Stretch.UniformToFill;
+                InputBox.Background = imageBrush;
+            }
+        }
+
+        private void input_Image(string Path, bool isFloder)
+        {
+            string DefaultImagePath = "pack://application:,,,/Images/FloderImg.png";//相对路径
+
+            if (isFloder)
+            {
+                InputTextBox.Text = Path;
+                // 设置 Border 的背景为默认文件夹图片
+                SetBorderBackground(DefaultImagePath);
+            }
+            else
+            {
+                InputTextBox.Text = Path;
+                SetBorderBackground(Path);
+            }
+        }
+
+        private void Images_File_DragEnter(object sender, DragEventArgs e)
         {
             Debug.WriteLine("拖拽图片进来了");
         }
 
-        private void Grid_Drop(object sender, DragEventArgs e)
+        private void Images_File_Drop(object sender, DragEventArgs e)
         {
-            Debug.WriteLine("放下图片了");
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files.Length > 0)
+                {
+                    // 获取拖拽的第一个文件路径
+                    string imagePath = files[0];
+
+                    if (File.Exists(imagePath))//是文件
+                    {
+                        input_Image(imagePath, false);
+                    }
+                    else if (Directory.Exists(imagePath))// 是文件夹
+                    {
+                        input_Image(imagePath, true);
+                    }
+                }
+            }
         }
 
-        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine("鼠标移动");
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "图片文件|*.jpg;*.jpeg;*.png;*.bmp;*.ico";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // 获取所选文件的路径
+                string selectedFilePath = openFileDialog.FileName;
+
+                input_Image(selectedFilePath, false);
+            }
         }
     }
 }
